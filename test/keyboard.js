2b1e4d2b6e38b43eeff2e45c3097beafd07c016c
Bertil Chapuis <bchapuis@gmail.com>
2020-10-15 15:03:09 +0200

    Example of a test for the keyboard

--- test/keyboard.js
+++ test/keyboard.js
@@ -1,1 +1,17 @@
 // TODO: Test the keyboard function or one of its components
+
+import { keyDown } from '../src/keyboard.js';
+
+const { assert } = require('chai');
+
+describe('keyboard.js', () => {
+  it('The keyDown function shoud filter duplicate events', () => {
+    let counter = 0;
+    const listener = () => {
+      counter += 1;
+    };
+    keyDown(listener, { key: 'ArrowLeft', preventDefault: () => {} });
+    keyDown(listener, { key: 'ArrowLeft', preventDefault: () => {} });
+    assert.equal(counter, 1);
+  });
+});
